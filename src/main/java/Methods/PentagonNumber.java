31e5b660969e757696bd6a80f621f4f4d8d0ea4d
grdharshini <gr.priyadharshini96@gmail.com>
2022-11-21 16:04:12 +0530

    last commit

---
+++ src/main/java/Methods/PentagonNumber.java
@@ -0,0 +1,16 @@
+package Methods;
+
+
+public class PentagonNumber {
+        public static void main(String[] args) {
+            int count = 1;
+            for(int i = 1; i <= 10; i++){
+                System.out.printf("%-6d",getPentagonalNumber(i));
+                if(count % 10 == 0) System.out.println();
+                count++;
+            }
+        }
+        public static int getPentagonalNumber(int i) {
+            return (i * (3 * i - 1))/2;
+        }
+    }
